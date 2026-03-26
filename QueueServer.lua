515e73bb839697d4f13d4edd87c0e86cb403eea1
tanveerisofficial-lang <infernoisofficial@gmail.com>
2026-03-26 23:36:29 +0530

    More work into JoinParty() and LeaveParty()

--- QueueServer.lua
+++ QueueServer.lua
@@ -2,25 +2,42 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 
---// Variables
+--// Modules
 local ZoneModule = require(ReplicatedStorage.Modules.Zone)
 
 --// Objects
 local TeleportZones = game.Workspace:WaitForChild("TeleportZones")
 local Configuration = TeleportZones.Configuration
 
-function JoinParty(Plr,ZoneContainer)
+--// Modules
+local ZoneData = {}
+
+function JoinParty(Plr,ZoneContainer,TpZones)
+	-- Add player to the lists
+	table.insert(ZoneData[TpZones]["Players"],Plr)
+	-- Tp Character into box
 	local Char = Plr.Character or Plr.CharacterAdded:Wait()
 	local RootPart = Char:WaitForChild("HumanoidRootPart")
 	RootPart.CFrame = ZoneContainer.CFrame
 end
 
-function LeaveParty()
-	
+function LeaveParty(Plr,LobbyPos,TpZones)
+	-- Remove player from the lists
+	table.remove(ZoneData[TpZones]["Players"],table.find(ZoneData[TpZones]["Players"],Plr)
+	-- Tp Character out of box
+	local Char = Plr.Character or Plr.CharacterAdded:Wait()
+	local RootPart = Char:WaitForChild("HumanoidRootPart")
+	RootPart.CFrame = TeleportZones.LobbyPos.CFrame
 end
 
 for _, TpZones in pairs(TeleportZones:GetChildren()) do
 	if TpZones:IsA("Model") then
+
+		ZoneData[TpZones] = {
+			["IsReady"] = false,
+			["MaxPlayers"] = 0,
+			["Players"] = {},
+		}
 
 		local ZoneContainer = TpZones:FindFirstChild("ZoneContainer")
 		if not ZoneContainer or not ZoneContainer:IsA("BasePart") then
